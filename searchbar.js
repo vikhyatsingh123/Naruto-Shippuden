a881cd648b77eaac74227cdd1c31304a1c86e83a
Dennis Braun <dbraun.webdev@gmail.com>
2021-10-29 13:40:19 +0200

    fix(formatting): fixed formatting for js and html files

--- searchbar.js
+++ searchbar.js
@@ -2,39 +2,37 @@ const icon = document.querySelector('.icon');
 const search = document.querySelector('.search');
 
 
-icon.addEventListener('click',()=>{
+icon.addEventListener('click', () => {
     search.classList.toggle('activated');
 })
 
 
 const search_box = document.querySelector('#mysearch');
 
-search_box.addEventListener('input',()=>{
+search_box.addEventListener('input', () => {
 
     window.scrollTo(0, 1383.33);
-    
-   const filter = search_box.value.toUpperCase(); 
-    
-   
-    let container =  document.getElementById('cont');
+
+    const filter = search_box.value.toUpperCase();
+
+
+    let container = document.getElementById('cont');
     let cont = container.getElementsByClassName('card')
-   
-     for(let i=0;i<cont.length;i++)
-     {
+
+    for (let i = 0; i < cont.length; i++) {
         const h = cont[i].getElementsByTagName('h2')[0];
 
         const textValue = h.textContent.toUpperCase()
-      
 
-        if(textValue.includes(filter))
-        {
+
+        if (textValue.includes(filter)) {
             cont[i].style.display = 'block'
         }
-        else{
+        else {
             cont[i].style.display = 'none';
         }
-     }
-   
-   
+    }
+
+
 
 })
