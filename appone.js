55e71b1122738165454166d255c0a45f01b7fb7c
Chaitanya Rai <95732261+Chaitanyarai899@users.noreply.github.com>
2022-10-24 21:30:04 +0530

    Create appone.js

---
+++ appone.js
@@ -0,0 +1,59 @@
+// react code for a front end application.
+import React from "react";
+import { useState, useEffect } from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+
+// bcf60fa8
+const API_URL = "https://www.omdbapi.com?apikey=bcf60fa8";
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
+  
+    useEffect(() => {
+      searchMovies("Batman");
+    }, []);
+  
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+  
+      setMovies(data.Search);
+    };
+  
+    return (
+      <div className="app">
+        <h1>MovieLand</h1>
+  
+        <div className="search">
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for movies"
+          />
+          <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
+  
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default App;
