4d1c9226311e2c877340ad2d01a3fcbe0ad179af
codermonkvipin <114944948+codermonkvipin@users.noreply.github.com>
2022-10-31 22:01:56 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div className='about'>
+      <h1>About Me</h1>
+      <p className='w'><span className='m'>M</span>  <span className='me'>yself Anuj Kumar curently pursuing my B.Tech course in the feild of Electronics and Communication engineering from Sant Longowal Institute of Engineering and Technology,Punjab</span> </p>
+      <h1>Contact Us</h1>
+      <p className='w'><span className='m'>C</span><span className='me'>ontact us if you have any query regarding Site ,Blogand any other issue , please feel to free at---> <span className='s'>anujkumar65638@gmail.com</span></span></p>
+      <h1>Technology Used</h1>
+      <p className='w'><span className='m'>M</span><span className='me'>king this the Technology used ---> <span className='s'>React Js , Mongodb , Express Js , Node Js and CSS. </span>This is a complete MERN project.</span></p>
+      <h1>Prevent Abuse</h1>
+      <p className='w'><span className='m'>W</span><span className='me'>ith a Terms and Conditions,you can outline the behaviors that you prohibite on website ,such as harassing and spamming other users or posting defamatory content.</span></p>
+      <h1>Other</h1>
+      <p className='w'><span className='m'>G</span><span className='me'>it Hub Link---> <span className='p'> <a href="https://github.com/Buildwithus"> Click here</a> </span></span></p>
+      <p className='w'><span className='m'>L</span><span className='me'>inkedin Link---> <span className='p'> <a href="https://www.linkedin.com/in/anuj-kumar-756826225"> Click here</a> </span></span></p>
+      <p className='w'><span className='m'>N</span><span className='me'>etlify Link---> <span className='p'><a href='https://app.netlify.com/teams/anujkumar666768/overview?_ga=2.188322350.781077239.1660938029-99913874.1660938029'>Click here</a></span> </span></p>
+      <h1>Thank You!</h1>
+      <h1></h1>
+      
+      
+    </div>
+  )
+}
+
+export default about
