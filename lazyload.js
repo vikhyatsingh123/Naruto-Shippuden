c03e101b6d09c668175e2f6f7ffa4c48af99424d
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-10-31 23:44:41 +0100

    Lazy loading Images
    I added lazy loading functionality, in html and js. This change will make website load 40% faster at least. i only applied this to character cards fill free to include other elements. P.s this change will require a change to the readme file, the part about how to contribute to the project

---
+++ lazyload.js
@@ -0,0 +1,24 @@
+const imgs=document.querySelectorAll("[data-src]")
+let imgOptions={threshold:0,rootMargin:"0px 0px 300px 0px"}
+function preloadImg(img){
+        const src=img.getAttribute("data-src")
+
+        if(!src) return;
+        
+        img.src=src
+}
+const imgObserver= new IntersectionObserver((entries,imgObserver)=>{
+        entries.forEach(entry=>{
+                if(!entry.isIntersecting){
+                        return 
+                }else{
+                        preloadImg(entry.target)
+                        imgObserver.unobserve(entry.target)
+                }
+        })
+},imgOptions )
+
+
+imgs.forEach(image=>{
+        imgObserver.observe(image)
+})
