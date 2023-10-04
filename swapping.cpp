bd81d88c5bca63a7afc015b4d06ec302620c6f0f
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-04 09:36:54 +0530

    swapping

---
+++ swapping.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int a = 5, b = 10;
+
+    std::cout << "Before swapping:" << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    std::cout << "After swapping:" << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
