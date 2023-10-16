7b5f6784e5a1338b94dbbd8a9e42e0a5dcaf1c05
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-10-16 17:06:21 +0530

    Create Tringle.js

---
+++ Tringle.js
@@ -0,0 +1,43 @@
+
+// Javascript program for the above approach
+
+// Function to count all possible
+// triangles with arr[] elements
+function findNumberOfTriangles(arr, n)
+{
+
+	// Count of triangles
+	let count = 0;
+
+	// The three loops select three
+	// different values from array
+	for (let i = 0; i < n; i++) 
+	{
+		for (let j = i + 1; j < n; j++)
+		{
+
+			// The innermost loop checks for
+			// the triangle property
+			for (let k = j + 1; k < n; k++)
+
+				// Sum of two sides is greater
+				// than the third
+				if (
+					arr[i] + arr[j] > arr[k]
+					&& arr[i] + arr[k] > arr[j]
+					&& arr[k] + arr[j] > arr[i])
+					count++;
+		}
+	}
+	return count;
+}
+
+// Driver Code
+	let arr = [ 10, 21, 22, 100, 101, 200, 300 ];
+	let size = arr.length;
+	
+	document.write( "Total number of triangles possible is "+
+	findNumberOfTriangles(arr, size));
+
+// This code is contributed by souravghosh0416.
+
